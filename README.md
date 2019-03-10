ae47ec5eb5b8d7d1e81201d754d02857db09ca6f
Will_tam <will_tam@club-internet.fr>
2019-03-10 14:09:20 +0100

    Pypi pakg preparation

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-** README **
-
-
-__Read /var/log/apt/history.log.* and display it usefully__
-
-
-Take a look in CHANGELOG.md.
-
-Try to not fill to much memory, for small devices. So often read files, event if it's slower.
-
-Please, if you want something more complete, look at https://github.com/terminalforlife/apt-undo-install/blob/master/apt-undo-install side
+/home/will/data/programmation/python/apt-histo/apt-histo/README.md
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("VERISION", "r") as fh:
+    version = fh.read()
+
+setuptools.setup(
+    name="apt-histo",
+    version=version,
+    author="will",
+    author_email="will_tam@club-internet.fr",
+    description="Read /var/log/apt/history.log.* and display it usefully",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/will_tam-bash/apt-histo.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: System :: Archiving :: Packaging",
+    ],
+)
